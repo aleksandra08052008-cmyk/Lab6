dbfe6d27166570083e51e6eeff0639e67e71c266
aleksandra08052008-cmyk <aleksandra08052008@gmail.com>
2026-05-22 19:15:35 +0400

    Create Main.java

---
+++ src/main/java/collection/Main.java
@@ -0,0 +1,38 @@
+package collection;
+
+import java.util.List;
+
+/**
+ * Виконавчий клас програми.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedSet<Integer> set1 = new DoublyLinkedSet<>();
+
+        set1.add(10);
+        set1.add(20);
+        set1.add(10);
+
+        System.out.println("Set1: " + set1);
+
+        DoublyLinkedSet<String> set2 =
+                new DoublyLinkedSet<>("Java");
+
+        System.out.println("Set2: " + set2);
+
+        DoublyLinkedSet<Integer> set3 =
+                new DoublyLinkedSet<>(List.of(1, 2, 3, 4, 4));
+
+        System.out.println("Set3: " + set3);
+
+        set3.remove(2);
+
+        System.out.println("After remove: " + set3);
+
+        System.out.println("Contains 3: " + set3.contains(3));
+
+        System.out.println("Size: " + set3.size());
+    }
+}
