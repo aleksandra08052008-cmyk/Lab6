b20a4663541d8b6ecfa9b004174876e7afb06bfb
aleksandra08052008-cmyk <aleksandra08052008@gmail.com>
2026-05-22 19:35:59 +0400

    Update Node.java

--- src/main/java/collection/Node.java
+++ src/main/java/collection/Node.java
@@ -1,21 +1,12 @@
 package collection;
 
-/**
- * Вузол двозв'язного списку.
- *
- * @param <T> тип елементу
- */
 class Node<T> {
 
     T data;
     Node<T> next;
     Node<T> previous;
 
-    /**
-     * Конструктор вузла.
-     *
-     * @param data значення вузла
-     */
+   
     Node(T data) {
         this.data = data;
     }
